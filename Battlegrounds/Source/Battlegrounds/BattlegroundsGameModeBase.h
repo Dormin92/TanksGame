f93fa8e43e041300dae15d9b4cafb5d8f92017ec
Dormin92 <45270590+Dormin92@users.noreply.github.com>
2020-01-13 00:06:39 +0000

    Tank added with some UI

---
+++ Battlegrounds/Source/Battlegrounds/BattlegroundsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattlegroundsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEGROUNDS_API ABattlegroundsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
